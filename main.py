51799afd677f9640b86f98e4a497248023dd0426
wasif-amin <166426702+wasif-amin@users.noreply.github.com>
2025-10-03 18:56:02 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from  sqlalchemy import select
+
+
+app = Flask(__name__)
+
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+all_books = []
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    result = db.session.execute(select(Book).order_by(Book.title))
+    all_books = result.scalars().all()
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        # NEW CODE: Create a Book object and save it to the database
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=float(request.form["rating"])  # Always convert rating to float/number
+        )
+
+        db.session.add(new_book)
+
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    return render_template("add.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+
